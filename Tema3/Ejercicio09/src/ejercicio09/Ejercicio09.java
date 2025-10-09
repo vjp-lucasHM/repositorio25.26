fd6de612c91297881158c2075c22f450c8420086
1DAM - Lucas Herrera Moreno <lherreram06@informatica.iesvalledeljerteplasencia.es>
2025-10-09 19:11:52 +0200

    more comments

--- Tema3/Ejercicio09/src/ejercicio09/Ejercicio09.java
+++ Tema3/Ejercicio09/src/ejercicio09/Ejercicio09.java
@@ -20,35 +20,36 @@ public class Ejercicio09 {
         int num1, num2, num3, num4;
         
         System.out.println("Por favor, introduce el primer numero:");
-        num1 = scanner.nextInt();
+        num1 = scanner.nextInt(); // Pedimos el primer numero
         
         System.out.println("Por favor, introduce el segundo:");
-        num2 = scanner.nextInt();
+        num2 = scanner.nextInt(); // Pedimos el segundo numero
         
         System.out.println("Introduce el tercer numero:");
-        num3 = scanner.nextInt();
+        num3 = scanner.nextInt(); // Pedimos el tercer numero
         
         System.out.println("Por favor, introduce el cuarto numero:");
-        num4 = scanner.nextInt();
+        num4 = scanner.nextInt(); // Pedimos el cuarto numero
         
         if(num1 > num2) {
             int aux = num2;
             num2 = num1;
             num1 = aux;
-        }
+        } // Hacemos la primera comparacion
         
         if(num2 > num3) {
             int aux = num3;
             num3 = num2;
             num2 = aux;
-        }
+        } // Hacemos la segunda comparacion
         
         if(num3 > num4) {
             int aux = num4;
             num4 = num3;
             num3 = aux;
-        }
+        } // Hacemos la tercera comparacion
         
+        // Mandamos el mensaje final a la consola
         System.out.println("El orden de los numeros introducidos es el siguiente: " + num1 + " - " + num2 + " - " + num3 + " - " + num4);
     }
     
