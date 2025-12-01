dfb6cd03827ce01af720103f42f0ce28adcdf3e1
1DAM - Lucas Herrera Moreno <lherreram06@informatica.iesvalledeljerteplasencia.es>
2025-12-01 20:50:49 +0100

    start ejercicio1 t6

---
+++ Tema6/Ejercicio1/src/ejercicio1/Calculadora.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author alumno
+ */
+public abstract class Calculadora {
+    
+    private int numero;
+
+    public Calculadora() {
+        this.numero = 0;
+    }
+
+    public Calculadora(int numero) {
+        this.numero = numero;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    
+    
+    public abstract int realizarOperacion(int num1, int num2);
+    
+}
---
+++ Tema6/Ejercicio1/src/ejercicio1/Suma.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio1;
+
+/**
+ *
+ * @author alumno
+ */
+public class Suma extends Calculadora {
+
+    @Override
+    public int realizarOperacion(int num1, int num2) {
+        return num1+num2;
+    }    
+}
