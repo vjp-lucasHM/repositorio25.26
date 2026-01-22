8f6c4a1dda3fbb6240bacfa546fdb8ed9fe71fd7
1DAM - Lucas Herrera Moreno <lherreram06@informatica.iesvalledeljerteplasencia.es>
2026-01-22 16:10:17 +0100

    16 17

--- Entornos/Ejercicio16/src/ejercicio16/Ejercicio16.java
+++ Entornos/Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -12,10 +12,9 @@ import java.util.Scanner;
  * @author alumno
  */
 public class Ejercicio16 {
+    
+    String[] asignaturas = {"Lengua", "Mates", "Historia", "Física"};
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         int[][] matriz = new int[6][4];
         boolean exit = false;
@@ -32,10 +31,13 @@ public class Ejercicio16 {
                     mostrarNotas(matriz);
                     break;
                 case 3:
+                    hacerMejorMedia(matriz);
                     break;
                 case 4:
+                    getAlumnoMasSuspensos(matriz);
                     break;
                 case 5:
+                    sacarAsignaturaMasDificil(matriz);
                     break;
                 case 6:
                     System.out.println("Saliendo del programa...");
@@ -45,8 +47,53 @@ public class Ejercicio16 {
         } while(!exit);
     }
     
-    public static int hacerMediaAlumnos(int[][] matriz) {
-        return 0;
+    public static void hacerMejorMedia(int[][] matriz) {
+        
+        String[] alumnos = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
+        
+        int mejorMedia = 0;
+        String mejorAlumno = "";
+        
+        for (int i = 0; i < matriz.length; i++) {
+            double suma = 0;
+            
+            for(int j = 0; j < matriz[i].length; j++) {
+                suma += matriz[i][j];
+            }
+            
+            int media = (int) suma / matriz.length;
+            
+            if(media > mejorMedia) {
+                mejorMedia = media;
+                mejorAlumno = alumnos[i];
+            }
+        }
+        
+        System.out.println("El alumno con mejor nota es " + mejorAlumno + " con una media de " + mejorMedia);
+    }
+    
+    public static void getAlumnoMasSuspensos(int[][] matriz) {
+        int numSuspensos = -1000;
+        String alumno = "";
+        
+        String[] alumnos = {"Pepe", "Juan", "Ana", "Marta", "Pedro", "María"};
+        
+        for(int i = 0; i < matriz.length; i++) {
+            int suspensos = 0;
+            
+            for(int j = 0; j < matriz[i].length; j++) {
+                if(matriz[i][j] < 5) {
+                    suspensos++;
+                }
+            } 
+            
+            if(suspensos > numSuspensos) {
+                numSuspensos = suspensos;
+                alumno = alumnos[i];
+            }
+        }
+        
+        System.out.println("El alumno con mas suspensos es " + alumno + " con un total de " + numSuspensos + " suspensos.");
     }
     
     public static void mostrarNotas(int[][] matriz) {
@@ -55,6 +102,30 @@ public class Ejercicio16 {
                 System.out.println("[ Alumno Numero " + (i+1) + " - Asignatura Numero " + (j+1) + " ] -> " + matriz[i][j]);
             }
         }
+    }
+    
+    public static void sacarAsignaturaMasDificil(int[][] matriz) {
+        int peorMedia = 999;
+        String peor = "";
+        
+        String[] asignaturas = {"Lengua", "Mates", "Historia", "Fisica"};
+        
+        for(int i = 0; i < matriz.length; i++) {
+            int suma = 0;
+            
+            for(int j = 0; j < matriz[i].length; j++) {
+                suma += matriz[i][j];
+            }
+            
+            int media = suma / matriz.length;
+            
+            if(media < peorMedia) {
+                peorMedia = (int) media;
+                peor = asignaturas[i];  
+            }
+        }
+        
+        System.out.println("La asignatura con peor media es " + peor + " con una media de " + peorMedia);
     }
 
     public static int pedirOpcion() {
@@ -85,7 +156,7 @@ public class Ejercicio16 {
     public static void mostrarMenu() {
         System.out.println("1. Rellenar las notas de los alumnos.");
         System.out.println("2. Mostrar las notas introducidas en el punto anterior.");
-        System.out.println("Que nos diga que alumno es el mejor de la clase.");
+        System.out.println("3. Que nos diga que alumno es el mejor de la clase.");
         System.out.println("4. Que nos diga el alumno con mas suspensos.");
         System.out.println("5. Que nos diga cual es la asignatura mas dificil (nota media mas baja)");
     }
---
+++ Entornos/Ejercicio17/src/ejercicio17/Ejercicio17.java
@@ -0,0 +1,52 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio17;
+
+/**
+ *
+ * @author alumno
+ */
+public class Ejercicio17 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int[] vector = new int[10];
+        System.out.println("------------ NUMEROS SIN ORDENAR ----------------");
+        rellenarMatriz(vector);
+        mostrarNumeros(vector);
+        System.out.println("------------ NUMEROS ORDENADOS ----------------");
+        ordenarMayorMenor(vector);
+        mostrarNumeros(vector);
+    }
+
+    public static void rellenarMatriz(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            vector[i] = (int) (Math.random() * 10);
+        }
+    }
+
+    public static void mostrarNumeros(int[] vector) {
+        for (int i = 0; i < vector.length; i++) {
+            System.out.println(vector[i] + "");
+        }
+    }
+
+    public static void ordenarMayorMenor(int[] vector) {
+        for (int i = 0; i < vector.length - 1; i++) {
+            for (int j = 0; j < vector.length - 1; j++) {
+                
+                if (vector[j] < vector[j + 1]) {
+                    int aux = vector[j];
+                    vector[j] = vector[j + 1];
+                    vector[j + 1] = aux;
+                }
+                
+            }
+        }
+    }
+
+}
