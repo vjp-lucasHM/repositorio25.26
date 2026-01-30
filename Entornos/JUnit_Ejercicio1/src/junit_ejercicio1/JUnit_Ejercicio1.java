e926880e5d7fe8e5ce98d41bf70bd84b55b3a5a1
1DAM - Lucas Herrera Moreno <lherreram06@informatica.iesvalledeljerteplasencia.es>
2026-01-30 20:56:28 +0100

    ejercicio 1

---
+++ Entornos/JUnit_Ejercicio1/src/junit_ejercicio1/JUnit_Ejercicio1.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package junit_ejercicio1;
+/**
+ *
+ * @author alumno
+ */
+public class JUnit_Ejercicio1 {
+
+    
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
