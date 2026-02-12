8d02caeebf404867a1e32aa8835117ef5cd6c353
1DAM - Lucas Herrera Moreno <lherreram06@informatica.iesvalledeljerteplasencia.es>
2026-02-12 16:50:30 +0100

    ejercicio 5

--- Tema9/Ejercicio5/src/ejercicio5/Ejercicio5.java
+++ Tema9/Ejercicio5/src/ejercicio5/Ejercicio5.java
@@ -4,6 +4,8 @@
  */
 package ejercicio5;
 
+import java.util.Scanner;
+
 /**
  *
  * @author alumno
@@ -14,7 +16,28 @@ public class Ejercicio5 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String[] dias = {"Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabadoo", "Domingo"};
+        String[] numeros = {"primer", "segundo", "tercer", "cuarto", "quinto", "sexto", "septimo"};
+        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce un dia de la semana:");
+        String dia = scanner.nextLine();
+        
+        int indice = -1;
+        
+        for(int i = 0; i < dias.length; i++) {
+            if(dias[i].equalsIgnoreCase(dia)) {
+                indice = i;
+            }
+        }
+        
+        if(indice == -1) {
+            System.out.println("Has introducido un dia invalido. Intentalo otra vez.");
+        } else {
+            String numero = numeros[indice];
+            
+            System.out.println(dia + " es el " + numero + " dia de la semana.");
+        }
     }
     
 }
